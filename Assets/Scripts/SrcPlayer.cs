a2664248356b7d74eb548e0f1a73326de914a7be
David Muriel <murielcd@campusemav.com>
2020-10-19 09:57:37 +0200

    19-10-2020

--- Assets/Scripts/SrcPlayer.cs
+++ Assets/Scripts/SrcPlayer.cs
@@ -5,21 +5,57 @@ using UnityEngine;
 public class SrcPlayer : MonoBehaviour
 {
     [SerializeField]
+    float forsa = 3;
+    float x, y; //variables para guardar la lectura de los cursores 
     float velocitat = 3;
+
+    //acceder al componente RigidBody:
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x, y; // para leer gamepad
         x = Input.GetAxis("Horizontal"); //leer movimiento horizontal
-        y = Input.GetAxis("Vertical"); //leer movimiento vertical 
-        //print("Horizontal: " + x + "/Vertical: " + y); //mostramos x e y 
-        transform.Translate(x *Time.deltaTime*velocitat, y*Time.deltaTime*velocitat, 0);
+        y = Input.GetAxis("Vertical"); //leer movimiento vertical
+        
+
+
+
+
+
+
+
+
+      
     }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(new Vector2(x*forsa, y*forsa));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print(collision.name);
+        if (collision.tag == "pickup")
+        {
+            Destroy(collision.gameObject);
+            ScrControl.punt += 5;
+            print(ScrControl.punt);
+        }
+
+    }
+
+    
+    
+        
+    
 
 }
